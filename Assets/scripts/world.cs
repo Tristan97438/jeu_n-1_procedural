4355ce0f3db22436d39aaec5d50afb212548f428
Laurent Tristan <tl974.public@gmail.com>
2025-09-23 21:54:26 +0400

    début création terrain 2

--- Assets/scripts/world.cs
+++ Assets/scripts/world.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    
     [SerializeField] GameObject cube_prefabs;
-    public GameObject[] cube_monde = new GameObject[1000];
+    public int largeur = 100;
+    public int longueur = 100;
+    public GameObject[,] cube_monde; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for (int i = 0; i < 100; i++)
+        cube_monde = new GameObject[largeur, longueur];
+        for (int i = 0; i < largeur; i++)
         {
+            for(int j= 0 ; j < longueur; j++)
+            {
+                int hauteur = Random.Range(0, 3);
+                cube_monde[i, j] = Instantiate(cube_prefabs, new Vector3(i, hauteur, j), Quaternion.identity);
+                Renderer rend = cube_monde[i, j].GetComponent<Renderer>();
+
+                // Duplique le matériau pour éviter que tous les cubes partagent la même couleur
+                rend.material = new Material(rend.material);
+
+                // Couleur aléatoire
+                rend.material.color = new Color(Random.value, Random.value, Random.value);
+            }
             
         }
+        
+
     }
 
     // Update is called once per frame
