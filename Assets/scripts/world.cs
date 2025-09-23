369a893fd1ea60fb8f30709b3f0c74c31ded6449
Laurent Tristan <tl974.public@gmail.com>
2025-09-23 22:20:05 +0400

    début score

--- Assets/scripts/world.cs
+++ Assets/scripts/world.cs
@@ -4,6 +4,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     
     [SerializeField] GameObject cube_prefabs;
+    [SerializeField] TMPro.TextMeshPro "text (TMP)";
     public int largeur = 100;
     public int longueur = 100;
     public GameObject[,] cube_monde; 
@@ -15,7 +16,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
         {
             for(int j= 0 ; j < longueur; j++)
             {
-                int hauteur = Random.Range(0, 3);
+                int hauteur = Random.Range(0, 5);
                 cube_monde[i, j] = Instantiate(cube_prefabs, new Vector3(i, hauteur, j), Quaternion.identity);
                 Renderer rend = cube_monde[i, j].GetComponent<Renderer>();
 
