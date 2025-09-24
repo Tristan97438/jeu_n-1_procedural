d68bd195bf669bf5958d4730fa5f743032241ea8
Laurent Tristan <t.laurent@rubika-edu.re>
2025-09-24 15:44:30 +0400

    algo labyrinthe assez bien avancer

--- Assets/scripts/world.cs
+++ Assets/scripts/world.cs
@@ -66,124 +66,242 @@ public class World: MonoBehaviour
         //on enregistre dans l'historique de nos cases dans le tableau --> historique_case_visiter
         historique_case_visiter[compteur_case] = position_en_cours;
 
-        //-------boucle-------
-
-        //on enregistre les cases voisines non visiter
-        GameObject[] cases_voisines = new GameObject[4];
-
-        //on regarde les cases qui sont a coté de celle ou nous sommes dans la création. 
-        //on ajoute 1 a notre compteur pour chaque case non visiter
-        if (case_visiter_creation[g, h + 1] == false)
+        int sorti_while = 0;
+        //-------boucle-------//
+        while(sorti_while < longueur*largeur* 5)
         {
-            //on incrémente notre tableau --> cases_voisines avec une case voisines non visiter
-            cases_voisines[compteur_case_non_visiter] = cube_monde[g, h + 1];
-            compteur_case_non_visiter += 1;
-            
-            
-        }
-        if (case_visiter_creation[g, h - 1] == false)
-        {
-            cases_voisines[compteur_case_non_visiter] = cube_monde[g, h + 1];
-            compteur_case_non_visiter += 1;
-
-            
-        }
-        if (case_visiter_creation[g + 1, h] == false)
-        {
-            cases_voisines[compteur_case_non_visiter] = cube_monde[g, h + 1];
-            compteur_case_non_visiter += 1;
-
-            
-        }
-        if (case_visiter_creation[g - 1, h] == false)
-        {
-            cases_voisines[compteur_case_non_visiter] = cube_monde[g, h + 1];
-            compteur_case_non_visiter += 1;
-
-            
-        }
-        //on regarde combien il y a de cases voisines non visiter
-        //si il y a 0 on revient a la case d'avant
-        if (compteur_case_non_visiter == 0)
-        {
-            case_visiter_creation[g, h] = true;
-            //on rajoute a l'historique la case actuelle
-            historique_case_visiter[compteur_case] = position_en_cours;
-            position_en_cours = historique_case_visiter[compteur_case - 1];
-            
-            
-            //on rajoute 1 au compteur_case 
-            compteur_case += 1;
-        }
-
-        if(compteur_case_non_visiter == 1)
-        {
-            case_visiter_creation[g, h] = true;
-            historique_case_visiter[compteur_case] = position_en_cours;
-            position_en_cours = cases_voisines[0];
-            compteur_case += 1;
-            // Trouver les coordonnées (i, j) de la nouvelle position
-            //signifient que les variables i et j sont initialisées à une valeur invalide par défaut (ici -1), pour signaler qu’on n’a pas encore trouvé les coordonnées recherchées.
-            int i = -1;
-            int j = -1;
-            //la case voisine
-            GameObject nouveau = cases_voisines[0];
-            for (int x = 0; x < largeur; x++)
+            //on créer un tableau dans lequel l'on va enregistre les cases voisines non visiter.
+            GameObject[] cases_voisines = new GameObject[4];
+            compteur_case_non_visiter = 0;
+            //on regarde les cases qui sont a coté de celle ou nous sommes dans la création. 
+            //on ajoute 1 a notre compteur pour chaque case non visiter
+            if (g==0 && h == 0)
             {
-                for (int y = 0; y < longueur; y++)
+                if (h + 1 < longueur && case_visiter_creation[g, h + 1] == false)
                 {
-                    if (cube_monde[x, y] == nouveau)
-                    {
-                        i = x;
-                        j = y;
-                        break;
-                    }
+                    //on incrémente notre tableau --> cases_voisines avec une case voisines non visiter
+                    cases_voisines[compteur_case_non_visiter] = cube_monde[g, h + 1];
+                    compteur_case_non_visiter += 1;
+
+
                 }
-                if (i != -1) break;
+                if (g + 1 < largeur && case_visiter_creation[g + 1, h] == false)
+                {
+                    cases_voisines[compteur_case_non_visiter] = cube_monde[g + 1 , h ];
+                    compteur_case_non_visiter += 1;
+
+
+                }
+            }
+            else{
+                if (h+1<longueur && case_visiter_creation[g, h + 1] == false)
+                {
+                    //on incrémente notre tableau --> cases_voisines avec une case voisines non visiter
+                    cases_voisines[compteur_case_non_visiter] = cube_monde[g, h + 1];
+                    compteur_case_non_visiter += 1;
+
+
+                }
+                if (h-1 >=0 && case_visiter_creation[g, h - 1] == false)
+                {
+                    cases_voisines[compteur_case_non_visiter] = cube_monde[g, h - 1];
+                    compteur_case_non_visiter += 1;
+
+
+                }
+                if (g + 1 < largeur && case_visiter_creation[g + 1, h] == false)
+                {
+                    cases_voisines[compteur_case_non_visiter] = cube_monde[g +1 , h ];
+                    compteur_case_non_visiter += 1;
+
+
+                }
+                if (g-1 >=0 && case_visiter_creation[g - 1, h] == false)
+                {
+                    cases_voisines[compteur_case_non_visiter] = cube_monde[g - 1 , h];
+                    compteur_case_non_visiter += 1;
+
+
+                }
+            }
+            
+            //on regarde combien il y a de cases voisines non visiter
+            //si il y a 0 on revient a la case d'avant
+            if (compteur_case_non_visiter == 0)
+            {
+                case_visiter_creation[g, h] = true;
+                //on rajoute a l'historique la case actuelle
+                historique_case_visiter[compteur_case] = position_en_cours;
+
+                if(compteur_case>0)
+                    position_en_cours = historique_case_visiter[compteur_case - 1];
+
+                if(compteur_case==0)
+                    position_en_cours = historique_case_visiter[compteur_case];
+
+
+                //on rajoute 1 au compteur_case 
+                compteur_case += 1;
             }
 
-            // Comparaison pour savoir la direction
-            if (i == g && j == h + 1)
+            if (compteur_case_non_visiter == 1)
             {
-                Debug.Log("NORD");
-                position_en_cours.GetComponent<Case>().OuvrirMurN();
-                nouveau.GetComponent<Case>().OuvrirMurS();
-            }
-                
-            else if (i == g && j == h - 1)
-            {
-                Debug.Log("SUD");
-                position_en_cours.GetComponent<Case>().OuvrirMurS();
-                nouveau.GetComponent<Case>().OuvrirMurN();
-            }
-                
-            else if (i == g + 1 && j == h)
-            {
-                Debug.Log("EST");
-                position_en_cours.GetComponent<Case>().OuvrirMurE();
-                nouveau.GetComponent<Case>().OuvrirMurO();
-            }
-                
+                case_visiter_creation[g, h] = true;
+                historique_case_visiter[compteur_case] = position_en_cours;
 
-            else if (i == g - 1 && j == h)
-            {
-                Debug.Log("OUEST");
-                position_en_cours.GetComponent<Case>().OuvrirMurO();
-                nouveau.GetComponent<Case>().OuvrirMurE();
-            }
-                
+                // Trouver les coordonnées (i, j) de la nouvelle position
+                //signifient que les variables i et j sont initialisées à une valeur invalide par défaut (ici -1), pour signaler qu’on n’a pas encore trouvé les coordonnées recherchées.
+                int i = -1;
+                int j = -1;
+                //la case voisine
+                GameObject nouveau = cases_voisines[0];
+                for (int x = 0; x < largeur; x++)
+                {
+                    for (int y = 0; y < longueur; y++)
+                    {
+                        if (cube_monde[x, y] == nouveau)
+                        {
+                            i = x;
+                            j = y;
+                            break;
+                        }
+                    }
+                    if (i != -1) break;
+                }
 
-            // Mise à jour des coordonnées
-            g = i;
-            h = j;
+                // Comparaison pour savoir la direction
+                //&& --> et
+                if (i == g && j == h + 1)
+                {
+                    Debug.Log("NORD");
+                    //on ouvre les murs
+                    position_en_cours.GetComponent<Case>().OuvrirMurN();
+                    nouveau.GetComponent<Case>().OuvrirMurS();
+                }
+
+                else if (i == g && j == h - 1)
+                {
+                    Debug.Log("SUD");
+                    //on ouvre les murs
+                    position_en_cours.GetComponent<Case>().OuvrirMurS();
+                    nouveau.GetComponent<Case>().OuvrirMurN();
+                }
+
+                else if (i == g + 1 && j == h)
+                {
+                    Debug.Log("EST");
+                    //on ouvre les murs
+                    position_en_cours.GetComponent<Case>().OuvrirMurE();
+                    nouveau.GetComponent<Case>().OuvrirMurO();
+                }
+
+
+                else if (i == g - 1 && j == h)
+                {
+                    Debug.Log("OUEST");
+                    //on ouvre les murs
+                    position_en_cours.GetComponent<Case>().OuvrirMurO();
+                    nouveau.GetComponent<Case>().OuvrirMurE();
+                }
+
+
+                // Mise à jour des coordonnées
+                g = i;
+                h = j;
+
+                position_en_cours = cases_voisines[0];
+                compteur_case += 1;
+            }
+
+            if (compteur_case_non_visiter > 1)
+            {
+                case_visiter_creation[g, h] = true;
+                historique_case_visiter[compteur_case] = position_en_cours;
+
+                int i = -1;
+                int j = -1;
+
+                int rand = Random.Range(0, compteur_case_non_visiter);
+                GameObject nouveau = cases_voisines[rand];
+                for (int x = 0; x < largeur; x++)
+                {
+                    for (int y = 0; y < longueur; y++)
+                    {
+                        if (cube_monde[x, y] == nouveau)
+                        {
+                            i = x;
+                            j = y;
+                            break;
+                        }
+                    }
+                    if (i != -1) break;
+                }
+
+                // Comparaison pour savoir la direction
+                //&& --> et
+                if (i == g && j == h + 1)
+                {
+                    Debug.Log("NORD");
+                    //on ouvre les murs
+                    position_en_cours.GetComponent<Case>().OuvrirMurN();
+                    nouveau.GetComponent<Case>().OuvrirMurS();
+                }
+
+                else if (i == g && j == h - 1)
+                {
+                    Debug.Log("SUD");
+                    //on ouvre les murs
+                    position_en_cours.GetComponent<Case>().OuvrirMurS();
+                    nouveau.GetComponent<Case>().OuvrirMurN();
+                }
+
+                else if (i == g + 1 && j == h)
+                {
+                    Debug.Log("EST");
+                    //on ouvre les murs
+                    position_en_cours.GetComponent<Case>().OuvrirMurE();
+                    nouveau.GetComponent<Case>().OuvrirMurO();
+                }
+
+
+                else if (i == g - 1 && j == h)
+                {
+                    Debug.Log("OUEST");
+                    //on ouvre les murs
+                    position_en_cours.GetComponent<Case>().OuvrirMurO();
+                    nouveau.GetComponent<Case>().OuvrirMurE();
+                }
+
+
+                // Mise à jour des coordonnées
+                if (i == -1 || j == -1)
+                {
+                    Debug.LogError("ERREUR : la case voisine sélectionnée n'a pas été trouvée dans la grille.");
+                    break; 
+                }
+                else
+                {
+                    g = i;
+                    h = j;
+                }
+
+                position_en_cours = cases_voisines[rand];
+                compteur_case += 1;
+
+
+            }
+            sorti_while += 1;
         }
         
 
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+
 }
+
+    
+    
