edc975b8ae51ec843871eadcbfee81f19af3f495
Laurent Tristan <t.laurent@rubika-edu.re>
2025-09-24 11:31:05 +0400

    début labyrinthe

--- Assets/scripts/Case.cs
+++ Assets/scripts/Case.cs
@@ -6,30 +6,28 @@ public class Case : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //OuvrirTousLesMurs();
+        
     }
 
-    public void OuvrirTousLesMurs()
-    {
-        for (int i = 0; i < murs.Length; i++)
-        {
-            murs[i].GetComponent<Mur>().ouvrir();
-        }
-    }
+    
     public void OuvrirMurN()
     {
+        //on ouvre la porte murs[0] c'est à dire la porte nord
         murs[0].GetComponent<Mur>().ouvrir();
     }
     public void OuvrirMurS()
     {
+        //on ouvre la porte murs[1] c'est à dire la porte Sud
         murs[1].GetComponent<Mur>().ouvrir();
     }
     public void OuvrirMurE()
     {
+        //on ouvre la porte murs[2] c'est à dire la porte Est
         murs[2].GetComponent<Mur>().ouvrir();
     }
     public void OuvrirMurO()
     {
+        //on ouvre la porte murs[3] c'est à dire la porte Ouest
         murs[3].GetComponent<Mur>().ouvrir();
     }
 
--- Assets/scripts/world.cs
+++ Assets/scripts/world.cs
@@ -5,9 +5,11 @@ public class World: MonoBehaviour
     
     [SerializeField] GameObject cube_prefabs;
    
-    public int largeur = 100;
-    public int longueur = 100;
-    public GameObject[,] cube_monde; 
+    int largeur = 100;
+    int longueur = 100;
+    GameObject[,] cube_monde;
+    //déclaration du tableau visiter qui va permettre de créer le labyrinthe
+    bool[,] case_visiter_creation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,8 +18,11 @@ public class World: MonoBehaviour
         {
             for(int j= 0 ; j < longueur; j++)
             {
-                int hauteur = Random.Range(0, 5);
-                cube_monde[i, j] = Instantiate(cube_prefabs, new Vector3(i, hauteur, j), Quaternion.identity);
+                //créer les cubes du labyrinthe
+                cube_monde[i, j] = Instantiate(cube_prefabs, new Vector3(i*2, 0, j*2), Quaternion.identity);
+                //augmentation de la taille des cubes
+                cube_monde[i, j].transform.localScale = new Vector3(2, 2, 2);
+                //récupéré le renderer pour appliquer la couleur
                 Renderer rend = cube_monde[i, j].GetComponent<Renderer>();
 
                 // Duplique le matériau pour éviter que tous les cubes partagent la même couleur
@@ -28,8 +33,44 @@ public class World: MonoBehaviour
             }
             
         }
-        
+        for (int i = 0; i < largeur; i++)
+        {
+            for (int j = 0; j < longueur; j++)
+            {
+                //ont initialise le tableau avec false pour dire que l'on a pas encore visiter de cases
+                case_visiter_creation[i, j] = false;
+            }
 
+        }
+        //on choisi une case qui seras notre depart
+        case_visiter_creation[0, 0] = true;
+        for (int i = 0; i < largeur; i++)
+        {
+            for (int j = 0; j < longueur; j++)
+            {
+                //on créer un compteur pour regarder combien de case autour de celle visiter ne le sont pas
+                int compteur_case_nom_visiter = 0;
+                //on regarde les cases qui sont a coté de celle ou nous sommes dans la création.
+                //on ajoute 1 a notre compter pour chaque case non visiter
+                if (case_visiter_creation[i, j + 1] == false)
+                {                   
+                    compteur_case_nom_visiter += 1;
+                }
+                if (case_visiter_creation[i, j - 1] == false)
+                {                    
+                    compteur_case_nom_visiter += 1;
+                }
+                if (case_visiter_creation[i + 1, j] == false)
+                {                
+                    compteur_case_nom_visiter += 1;
+                }
+                if (case_visiter_creation[i - 1, j] == false)
+                {                 
+                    compteur_case_nom_visiter += 1;
+                }
+            }
+
+        }
     }
 
     // Update is called once per frame
